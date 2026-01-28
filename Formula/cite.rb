52a155fe6ef429d4d3d08baaa84bfd11ffa9bfe0
Matthew Finlayson <matthewf@allenai.org>
2026-01-28 13:17:35 -0800

    Fix cite formula checksum

--- Formula/cite.rb
+++ Formula/cite.rb
@@ -2,7 +2,7 @@ class Cite < Formula
   desc "Upgrade BibTeX citations to archival venues"
   homepage "https://github.com/mattf1n/cite"
   url "https://github.com/mattf1n/cite/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "062667b59f9b3e1a15ac89b16656cb7dabd427da5a9a3b8b50029c9bed1433f2"
+  sha256 "a339e0b6d0ca87186b386fcbab45e81fcfa84f3e577b529518d9749478c48628"
   license "MIT"
 
   depends_on "rust" => :build
