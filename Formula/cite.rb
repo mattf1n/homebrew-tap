03dfd8b662c35e503c9537af4081e49e0fdc0791
Matthew Finlayson <matthewf@allenai.org>
2026-01-28 13:21:44 -0800

    Use release tarball for cite

--- Formula/cite.rb
+++ Formula/cite.rb
@@ -1,8 +1,8 @@
 class Cite < Formula
   desc "Upgrade BibTeX citations to archival venues"
   homepage "https://github.com/mattf1n/cite"
-  url "https://github.com/mattf1n/cite/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "a339e0b6d0ca87186b386fcbab45e81fcfa84f3e577b529518d9749478c48628"
+  url "https://github.com/mattf1n/cite/releases/download/v0.1.0/cite-0.1.0.tar.gz"
+  sha256 "365164ae4dae280e9a1b68e0d1b018cfc16f9710698279aa3a439a1e84e47031"
   license "MIT"
 
   depends_on "rust" => :build
